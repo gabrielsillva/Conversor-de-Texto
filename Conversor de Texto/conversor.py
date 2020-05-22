48000dd2d6157ee0f2aeb9f4811cce78a6ddd31e
Gabriel Da Silva <gabriel.dossantos2411@gmail.com>
2020-05-21 22:00:52 -0300

    Update conversor.py

--- Conversor de Texto/conversor.py
+++ Conversor de Texto/conversor.py
@@ -1,4 +1,4 @@
-es = int(input('''Escolha o que deseja fazer com sua frase:;
+es = int(input('''Escolha o que deseja fazer com sua frase:
     (1) colocar frase em maiúsculo 
     (2) colocar frase em minúsculo
     (3) colocar a primeira letra em maiúsculo\n
