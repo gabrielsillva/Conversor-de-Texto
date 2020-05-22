16ba7ac64079d89ab3ddcaeb6c85429794a20b27
Gabriel Da Silva <gabriel.dossantos2411@gmail.com>
2020-05-21 21:52:03 -0300

    Update conversor.py

---
+++ Conversor de Texto/conversor.py
@@ -0,0 +1,15 @@
+es = int(input('''Escolha o que deseja fazer com sua frase:;
+    (1) colocar frase em maiúsculo 
+    (2) colocar frase em minúsculo
+    (3) colocar a primeira letra em maiúsculo\n
+    '''))
+text = str(input('Digite seu texto:'))
+if es == 1:
+    a = text.upper()
+    print(a)
+elif es == 2:
+    b = text.lower()
+    print(b)
+elif es == 3:
+    c = text.capitalize()
+    print(c)
